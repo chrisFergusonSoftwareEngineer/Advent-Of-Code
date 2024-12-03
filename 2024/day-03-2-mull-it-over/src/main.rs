1cb260c6c28df8c47020dcb21c4a864a1be69233
CI/CD Version Write NPE - ref: ferg_testing <>
2024-12-03 09:52:35 -0500

    adding day 3

---
+++ 2024/day-03-2-mull-it-over/src/main.rs
@@ -0,0 +1,47 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+extern crate regex;
+use regex::Regex;
+
+fn main() {
+    let re = Regex::new(r"(?:mul\()(\d+)(?:,)(\d+)(?:\))|(don't)(\(\))|(do)(\(\))").unwrap();
+    
+    let mut total = 0;
+    let mut enabled = true;
+
+    // probably needlessly complex unwrapping of line read-in
+    if let Ok(lines) = read_lines("./input.txt") {
+        for line in lines {
+            if let Ok(line_string) = line {
+    
+                for (_, [first, second]) in re.captures_iter(&line_string).map(|c| c.extract()) {
+                    let mut skip_this_match = false;
+                    if first == "don't" {
+                        enabled = false;
+                        skip_this_match = true;
+                    }
+                    if first == "do" {
+                        enabled = true;
+                        skip_this_match = true;
+                    }
+
+                    if !skip_this_match && enabled {
+                        total += first.parse::<i32>().unwrap() * second.parse::<i32>().unwrap();
+                    }
+                }
+            }
+        }
+    }
+
+    println!("{}", total);
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
